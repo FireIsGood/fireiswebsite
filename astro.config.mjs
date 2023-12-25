428d3c8140ea62ab39b5620cb98c2f85fda10599
FireIsGood <fireisadequate@gmail.com>
2023-12-25 03:18:29 -0800

    Add svelte and a greeter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from "astro/config";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-    server: { port: 1625 },
+  server: {
+    port: 1625
+  },
+  site: "https://main--unrivaled-daifuku-8bdb99.netlify.app/",
+  integrations: [svelte()]
 });
---
+++ src/components/greeter.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let greetings: string[];
+    const getRandomIndex = () => Math.floor(Math.random() * greetings.length);
+    let randomGreeting = greetings[getRandomIndex()];
+
+    function newGreeting() {
+        randomGreeting = greetings[getRandomIndex()];
+    }
+</script>
+
+<p>{randomGreeting}</p>
+<button on:click={newGreeting}>New greeting</button>
